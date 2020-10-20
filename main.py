fca9bc43521692ba6c323b1337c7acf07db8a61e
Daniel Lefevre <dpllefevre89@gmail.com>
2020-10-19 21:06:28 -0400

    Added search function using cursor and tweet cleanup function

--- main.py
+++ main.py
@@ -36,10 +36,42 @@ def download_twitter_data(keys):
     return api.trends_place(ny_code)[0]
 
 
+def search_tweets(keys):
+    auth = tweepy.AppAuthHandler(keys["API key"], keys["Secret key"])
+    api = tweepy.API(auth)
+    tweets = api.search("$TSLA", lang="en", count=50)
+    return tweets
+
+
+def cursor_search(keys):
+    auth = tweepy.AppAuthHandler(keys["API key"], keys["Secret key"])
+    api = tweepy.API(auth)
+    query = "$TSLA"
+    language = "en"
+    tweets_to_get = 50
+    new_tweets = tweepy.Cursor(api.search, q=query, lang=language)
+    tweet_list = [status.text for status in new_tweets.items(tweets_to_get)]
+    return tweet_list
+
+
+def cleanup_tweets(list_of_tweets):
+    """remove twitter link and whitespace"""
+    new_list = []
+    for tweet in list_of_tweets:
+        tweet = tweet.split("https://")[0]
+        tweet = tweet.replace("\n", " ")
+        new_list.append(tweet)
+    return new_list
+
+
 # Load api keys
 api_keys = get_api_keys(twitter_keys)
 # Download Twitter data
-data = download_twitter_data(api_keys)
+# data = download_twitter_data(api_keys)
+# tweets = search_tweets(api_keys)
+tweets = cursor_search(api_keys)
+cleaned_tweets = cleanup_tweets(tweets)
+print(cleaned_tweets)
 # Save data to file
-write_json_to_file(data, example_trends)
+write_json_to_file(tweets, example_trends)
 
